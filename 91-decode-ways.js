42238e75fde4d90fe059812a1c4bf86518be2371
Stanislav Sysoev <d4rkr00t@gmail.com>
2019-04-22 12:06:46 +1000

    medium: decide ways – dynamic programming solution

--- 91-decode-ways.js
+++ 91-decode-ways.js
@@ -6,39 +6,28 @@
 // Output: 3
 // Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
 
-function decode(s, cache, pos) {
-  if (pos >= s.length) {
-    return 1;
-  }
-
-  if (cache[pos] > -1) {
-    return cache[pos];
-  }
-
-  let totalWays = 0;
-
-  if (pos + 1 <= s.length && s[pos] !== "0") {
-    totalWays += decode(s, cache, pos + 1);
-  }
-
-  if (pos + 2 <= s.length) {
-    let d = +s.substr(pos, 2);
-    if (d > 0 && d < 27 && s[pos] !== "0") {
-      totalWays += decode(s, cache, pos + 2);
-    }
-  }
-
-  cache[pos] = totalWays;
-  return totalWays;
-}
-
 /**
  * @param {string} s
  * @return {number}
  */
-var numDecodings = function(s) {
-  let cache = new Array(s.length).fill(-1);
-  return decode(s, cache, 0);
+let numDecodings = function(s) {
+  let dp = new Array(s.length + 1).fill(0);
+  dp[0] = 1;
+  dp[1] = s[0] === "0" ? 0 : 1;
+
+  for (let i = 2; i <= s.length; i++) {
+    let oneDigit = Number(s.substr(i - 1, 1));
+    let twoDigits = Number(s.substr(i - 2, 2));
+
+    if (oneDigit >= 1) {
+      dp[i] += dp[i - 1];
+    }
+    if (twoDigits >= 10 && twoDigits <= 26) {
+      dp[i] += dp[i - 2];
+    }
+  }
+
+  return dp[s.length];
 };
 
 console.log(numDecodings("226"), 3);
