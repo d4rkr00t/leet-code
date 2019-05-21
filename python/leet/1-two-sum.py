e9dfd729cae7c2f4c93d51636d8b3249211de144
Stanislav Sysoev <d4rkr00t@gmail.com>
2019-05-21 20:36:20 +1000

    easy: two sum [python]

---
+++ python/leet/1-two-sum.py
@@ -0,0 +1,15 @@
+def twoSum(nums, target):
+    rec = {}  # { 2: 0 }
+
+    for x, num in enumerate(nums):  # x=1
+        diff = target - num  # 9 - 7 = 2
+        if diff in rec:  # True
+            return [rec[diff], x]
+        else:
+            rec[num] = x
+
+    return []
+
+
+print(twoSum([2, 7, 11, 15], 9))
+print(twoSum([-3, 4, 3, 90], 0))
