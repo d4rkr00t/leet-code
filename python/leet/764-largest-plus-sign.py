76e6d1de58011d401add12cdaa234a9b5df3b86e
Stanislav Sysoev <d4rkr00t@gmail.com>
2020-05-15 13:47:46 +1000

    medium: largest plus sign

---
+++ python/leet/764-largest-plus-sign.py
@@ -0,0 +1,40 @@
+# Largest Plus Sign
+# https://leetcode.com/problems/largest-plus-sign/
+# medium
+#
+# Time:  TBD
+# Space: TBD
+#
+# Solution:
+# TBD
+
+
+def orderOfLargestPlusSign(N: int, mines: [[int]]) -> int:
+    banned = {tuple(mine) for mine in mines}
+    dp = [[0] * N for _ in range(N)]
+    ans = 0
+
+    for r in range(N):
+        count = 0
+        for c in range(N):
+            count = 0 if (r,c) in banned else count+1
+            dp[r][c] = count
+
+        count = 0
+        for c in range(N-1, -1, -1):
+            count = 0 if (r,c) in banned else count+1
+            if count < dp[r][c]: dp[r][c] = count
+
+    for c in range(N):
+        count = 0
+        for r in range(N):
+            count = 0 if (r,c) in banned else count+1
+            if count < dp[r][c]: dp[r][c] = count
+
+        count = 0
+        for r in range(N-1, -1, -1):
+            count = 0 if (r, c) in banned else count+1
+            if count < dp[r][c]: dp[r][c] = count
+            if dp[r][c] > ans: ans = dp[r][c]
+
+    return ans
